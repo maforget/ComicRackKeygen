32222d6798c5c9bdf2410ee60c22438d573d7cc7
maforget <11904426+maforget@users.noreply.github.com>
2020-01-15 16:04:36 -0500

    Add project files.

---
+++ ComicRackKeygen/frmMain.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtValidation = new System.Windows.Forms.TextBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(47, 13);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(283, 20);
+            this.txtEmail.TabIndex = 1;
+            this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
+            // 
+            // txtValidation
+            // 
+            this.txtValidation.Location = new System.Drawing.Point(47, 42);
+            this.txtValidation.Name = "txtValidation";
+            this.txtValidation.ReadOnly = true;
+            this.txtValidation.Size = new System.Drawing.Size(283, 20);
+            this.txtValidation.TabIndex = 2;
+            this.txtValidation.TextChanged += new System.EventHandler(this.txtValidation_TextChanged);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(97, 75);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 3;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Key";
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(178, 75);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 7;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 109);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.txtValidation);
+            this.Controls.Add(this.txtEmail);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ComicRack Keygen";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtValidation;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
---
+++ ComicRackKeygen/frmMain.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Linq;
+using System.Xml;
+using System.Security.Cryptography;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmMain : Form
+    {
+        #region Properties
+        public string ValidationKey { get; set; }
+        public DateTime ValidationDate { get; set; }
+        DateTime FakeDate = DateTime.Parse("2100-01-01T00:00:00");
+        public string UserEmail { get; set; }
+
+        private XElement Key;
+        private XElement Email;
+        private XElement Date;
+        private XElement Config;
+        #endregion
+
+        #region Events
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            SaveToSettings();
+        }
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+            UserEmail = txtEmail.Text;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            LoadSettings();
+            txtEmail.Text = UserEmail;
+            txtValidation.Text = ValidationKey;
+        }
+
+        private void txtValidation_TextChanged(object sender, EventArgs e)
+        {
+            ValidationKey = txtValidation.Text;
+        }
+        #endregion
+
+        private void LoadSettings()
+        {
+            InitSettings();
+            DateTime dt;
+            UserEmail = Email.Value;
+            ValidationKey = Key.Value;
+            DateTime.TryParse(Date.Value, out dt);
+            ValidationDate = dt;
+        }
+
+        private void InitSettings()
+        {
+            if (File.Exists(SettingPath()))
+            {
+                Config = XElement.Load(SettingPath(), LoadOptions.PreserveWhitespace);
+            }
+            else
+            {
+                Config = new XElement("Settings",
+                new XAttribute(XNamespace.Xmlns + "xsi", "http://www.w3.org/2001/XMLSchema-instance"),
+                new XAttribute(XNamespace.Xmlns + "xsd", "http://www.w3.org/2001/XMLSchema"),
+                new XElement("UserEmail", ""),
+                new XElement("VK", ""),
+                new XElement("ValidationDate", ""));
+            }
+
+            Email = Config.Element("UserEmail") ?? new XElement("UserEmail", "");
+            Key = Config.Element("VK") ?? new XElement("VK", "");
+            Date = Config.Element("ValidationDate") ?? new XElement("ValidationDate", "");
+
+            if (string.IsNullOrEmpty(Email.Value))
+                Config.Add(Email);
+
+            if (string.IsNullOrEmpty(Date.Value))
+                Config.Add(Date);
+
+            if (string.IsNullOrEmpty(Key.Value))
+                Config.Add(Key);
+
+        }
+
+        private void GenerateKey()
+        {
+            if (!string.IsNullOrEmpty(txtEmail.Text))
+            {
+                var ValidationDate2 = DateTime.UtcNow;
+                ValidationDate = FakeDate;
+                ValidationKey = CreateHash(Environment.MachineName + this.txtEmail.Text.Trim() + ValidationDate);
+                txtValidation.Text = ValidationKey;
+            }
+        }
+
+        private bool VerifyKey()
+        {
+            if (string.IsNullOrEmpty(UserEmail) || !DateTime.Equals(ValidationDate, FakeDate))
+            {
+                return false;
+            }
+
+            return Verify(Environment.MachineName + UserEmail + ValidationDate, ValidationKey);
+        }
+
+        private void SaveToSettings()
+        {
+            if (VerifyKey())
+            {
+                try
+                {
+                    if (Config == null || string.IsNullOrEmpty(Email.Value) || string.IsNullOrEmpty(Key.Value)
+                        || string.IsNullOrEmpty(Date.Value))
+                    {
+                        InitSettings();
+                    }
+
+                    Email.Value = UserEmail;
+                    Key.Value = ValidationKey;
+                    Date.Value = ValidationDate.ToString("s");
+
+                    XmlWriterSettings xms = new XmlWriterSettings
+                    {
+                        Indent = true,
+                        OmitXmlDeclaration = false
+                    };
+
+                    using (XmlWriter xml = XmlWriter.Create(SettingPath(true), xms))
+                    {
+                        Config.WriteTo(xml);
+                        xml.Flush();
+                    }
+
+                    MessageBox.Show("Key Saved");
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            }
+            else
+            {
+                GenerateKey();
+                SaveToSettings();
+            }
+        }
+
+        private static string SettingPath(bool Save = false)
+        {
+            string folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            folder = Path.Combine(folder, "cYo");
+            folder = Path.Combine(folder, "ComicRack");
+
+            if (Save && !Directory.Exists(folder))
+                Directory.CreateDirectory(folder);
+
+            folder = Path.Combine(folder, "Config.xml");
+
+            return folder;
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        #region Password
+        private static readonly HashAlgorithm algorithm = new SHA1Managed();
+
+        public static byte[] CreateByteHash(string text)
+        {
+            return CreateByteHash(Encoding.UTF8.GetBytes(text));
+        }
+
+        public static byte[] CreateByteHash(byte[] text)
+        {
+            if (text.Length == 0)
+            {
+                return new byte[0];
+            }
+            return algorithm.ComputeHash(text);
+        }
+
+        public static string CreateHash(string text)
+        {
+            return Convert.ToBase64String(CreateByteHash(text));
+        }
+
+        public static bool Verify(string text, string hashValue)
+        {
+            return (!string.IsNullOrEmpty(hashValue) && hashValue.Equals(CreateHash(text)));
+        }
+        #endregion
+
+    }
+}
